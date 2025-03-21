826a78c05bb9e9bf2946450e775ce247fb20fd67
peri-carvalho <pericles.carvalho@precojusto.app.br>
2025-03-21 08:31:36 -0400

    User entity and resource

---
+++ src/main/java/com/webgradleproject/course/entities/User.java
@@ -0,0 +1,63 @@
+package com.webgradleproject.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User(){
+    }
+
+    public User(Long id, String nome, String email, String phone, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
+
