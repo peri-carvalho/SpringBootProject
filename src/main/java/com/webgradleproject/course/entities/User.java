cd2c408568463c2a6c74705ee310435679563991
peri-carvalho <pericles.carvalho@precojusto.app.br>
2025-03-21 11:44:11 -0400

    feat: Order, instant, ISO 8601 - p01

--- src/main/java/com/webgradleproject/course/entities/User.java
+++ src/main/java/com/webgradleproject/course/entities/User.java
@@ -3,6 +3,8 @@ package com.webgradleproject.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    //usuario possui lista de orders um para muitos
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -65,5 +71,20 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
 
