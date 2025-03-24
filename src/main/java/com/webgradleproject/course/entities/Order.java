9c8f617abfb2f576dcc99a5a1b7c37c550ab1437
peri-carvalho <pericles.carvalho@precojusto.app.br>
2025-03-24 14:42:05 -0400

    feat: product-order-item one-to-many association

--- src/main/java/com/webgradleproject/course/entities/Order.java
+++ src/main/java/com/webgradleproject/course/entities/Order.java
@@ -40,6 +40,7 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    //coleção order item
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
